bc29c176034bb7dd9c1d2dabaa91b2cb6624763c
Alberto Marcos Blasco <38374836+albertoomarcos@users.noreply.github.com>
2019-03-12 11:20:44 +0100

    Añadiendo vistas anidadas 1
    Creación del mvc de la ventana principal que contiene las imagenes y añadiendo esta vista anidada a App.view.xml

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -4,12 +4,6 @@ sap.ui.define([
 ], function (Controller,  MessageToast) { 
 	"use strict"; 
 	return Controller.extend("brait_app.controller.App", {
-		onShowHello : function () {
-	// show a native JavaScript alert
-		var oBundle = this.getView().getModel("i18n").getResourceBundle(); 
-		var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name"); 
-		var sMsg = oBundle.getText("helloMsg", [sRecipient]); // show message 
-		MessageToast.show(sMsg); 
-		}
+		
 	});
 }); 
---
+++ webapp/controller/VPImages.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([ 
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast" 
+], function (Controller,  MessageToast) { 
+	"use strict"; 
+	return Controller.extend("brait_app.controller.VPImages", {
+		onShowHello : function () {
+	// show a native JavaScript alert
+		var oBundle = this.getView().getModel("i18n").getResourceBundle(); 
+		var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name"); 
+		var sMsg = oBundle.getText("helloMsg", [sRecipient]); // show message 
+		MessageToast.show(sMsg); 
+		}
+	});
+}); 
