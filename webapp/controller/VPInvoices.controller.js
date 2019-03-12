6f9b785b9bf1dbb305888f51d14ba34249256bcd
Alberto Marcos Blasco <albertoo.marcos@gmail.com>
2019-03-12 12:17:14 +0000

    Añadidos formatos personalizados para las facturas, se mostrará si el pedido es nuevo, está enviado o ya finalizó

--- webapp/controller/VPInvoices.controller.js
+++ webapp/controller/VPInvoices.controller.js
@@ -1,10 +1,12 @@
 sap.ui.define([ 
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
-	"sap/ui/model/json/JSONModel"
-], function (Controller,  MessageToast, JSONModel) { 
+	"sap/ui/model/json/JSONModel",
+	"../model/formatterInvoices"
+], function (Controller,  MessageToast, JSONModel,formatter) { 
 	"use strict"; 
 	return Controller.extend("brait_app.controller.VPInvoices", {
+		formatter: formatter,
 		onInit : function () {
 			var oViewModel = new JSONModel({
 				currency: "EUR"
