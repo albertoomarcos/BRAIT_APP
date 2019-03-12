de882c9a94388d87002bd5e9a6db41d4f485849d
Alberto Marcos Blasco <albertoo.marcos@gmail.com>
2019-03-12 12:02:02 +0000

    Añaiendo Data Types a VPInvoices, ahora se muestra el tipo de moneda.

--- webapp/controller/VPInvoices.controller.js
+++ webapp/controller/VPInvoices.controller.js
@@ -1,9 +1,15 @@
 sap.ui.define([ 
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast" 
-], function (Controller,  MessageToast) { 
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], function (Controller,  MessageToast, JSONModel) { 
 	"use strict"; 
 	return Controller.extend("brait_app.controller.VPInvoices", {
-		
+		onInit : function () {
+			var oViewModel = new JSONModel({
+				currency: "EUR"
+			});
+			this.getView().setModel(oViewModel, "view");
+		}
 	});
 }); 
