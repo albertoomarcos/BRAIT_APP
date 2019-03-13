6e296f7c32abc99a0f11bdafa7f96f7281c40b37
Alberto Marcos Blasco <albertoo.marcos@gmail.com>
2019-03-13 16:55:05 +0000

    Añadido vista desplegable en invoices

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -4,6 +4,14 @@ sap.ui.define([
 ], function (Controller,  MessageToast) { 
 	"use strict"; 
 	return Controller.extend("brait_app.controller.App", {
-		
+	onItemSelected: function(oEvent) {
+			var oSelectedItem = oEvent.getParameter("listItem");
+			var oContext = oSelectedItem.getBindingContext();
+			var sPath = oContext.getPath();
+			var oProductDetailPanel = this.byId("productDetailsPanel");
+
+			oProductDetailPanel.bindElement({path : sPath });
+			this.byId("productDetailsPanel").setVisible(true); 
+	}
 	});
 }); 
--- webapp/controller/VPInvoices.controller.js
+++ webapp/controller/VPInvoices.controller.js
@@ -1,21 +1,21 @@
-sap.ui.define([ 
+sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
 	"sap/ui/model/json/JSONModel",
 	"../model/formatterInvoices",
 	"sap/ui/model/Filter",
 	"sap/ui/model/FilterOperator"
-], function (Controller,  MessageToast, JSONModel,formatter,Filter, FilterOperator) { 
-	"use strict"; 
+], function (Controller, MessageToast, JSONModel, formatter, Filter, FilterOperator) {
+	"use strict";
 	return Controller.extend("brait_app.controller.VPInvoices", {
 		formatter: formatter,
-		onInit : function () {
+		onInit: function () {
 			var oViewModel = new JSONModel({
 				currency: "EUR"
 			});
 			this.getView().setModel(oViewModel, "view");
 		},
-		onFilterInvoices : function (oEvent) {
+		onFilterInvoices: function (oEvent) {
 			// build filter array
 			var aFilter = [];
 			var sQuery = oEvent.getParameter("query");
@@ -27,6 +27,11 @@ sap.ui.define([
 			var oList = this.byId("invoiceList");
 			var oBinding = oList.getBinding("items");
 			oBinding.filter(aFilter);
+		},
+		onItemSelected: function (oEvent) {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("detail");
 		}
+
 	});
-}); 
+});
