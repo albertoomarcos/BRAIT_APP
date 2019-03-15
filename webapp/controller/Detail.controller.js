ff1ce547b01e2aae4acc4b04a4879c29deb4cfe8
Alberto Marcos Blasco <albertoo.marcos@gmail.com>
2019-03-15 11:41:23 +0000

    Añadidos controles personalizados

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/core/UIComponent",
-	"sap/ui/core/routing/History"
-], function (Controller, UIComponent, History) {
+	"sap/ui/core/routing/History",
+	"sap/m/MessageToast"
+], function (Controller, UIComponent, History,MessageToast) {
 	"use strict";
 	return Controller.extend("brait_app.controller.Detail", {
 		onInit: function () {
@@ -10,6 +11,7 @@ sap.ui.define([
 			oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
 		},
 		_onObjectMatched: function (oEvent) {
+			this.byId("rating").reset();
 			this.getView().bindElement({
 				path: "/Invoices/"+ oEvent.getParameter("arguments").pruebaPath,
 				model: "prueba"
@@ -25,6 +27,12 @@ sap.ui.define([
 				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 				oRouter.navTo("overview", true);
 			}
+		},
+		onRatingChange: function (oEvent) {
+			var fValue = oEvent.getParameter("value");
+			var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+			MessageToast.show(oResourceBundle.getText("ratingConfirmation", [fValue]));
 		}
 	});
 });
