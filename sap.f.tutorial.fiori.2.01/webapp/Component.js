878f4203b3d187e3e9cfe3ca493e27b4bd05a902
Alberto Marcos Blasco <albertoo.marcos@gmail.com>
2019-03-21 11:50:05 +0000

    Cambio en URL relativa (mas facil de cambiar para cuando tenga el json en web)

--- sap.f.tutorial.fiori.2.01/webapp/Component.js
+++ sap.f.tutorial.fiori.2.01/webapp/Component.js
@@ -17,8 +17,10 @@ sap.ui.define([
 
 			// set products demo model on this sample
 			//url=https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/products.json
-			
-			oProductsModel = new JSONModel(sap.ui.require.toUrl('sap/ui/demo/mock') + '/products.json');
+			//oProductsModel = new JSONModel(sap.ui.require.toUrl('sap/ui/demo/mock') + '/products.json'); //ESTO FUNCIONA
+			//oProductsModel = new JSONModel(sap.ui.require.toUrl('sap/ui/demo/mock') + '/products.json');
+			oProductsModel = new JSONModel();
+			oProductsModel.loadData("https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/products.json");
 			oProductsModel.setSizeLimit(1000);
 			this.setModel(oProductsModel, 'products');
 		}
