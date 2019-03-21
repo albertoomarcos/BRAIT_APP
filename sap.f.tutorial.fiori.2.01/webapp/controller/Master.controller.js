6e4d583ac32111276f8f0015c77ac17014ebb680
Alberto Marcos Blasco <albertoo.marcos@gmail.com>
2019-03-21 11:17:45 +0000

    "Step 4: Adding a Detail Page": Completado sin errores   :white_check_mark:

--- sap.f.tutorial.fiori.2.01/webapp/controller/Master.controller.js
+++ sap.f.tutorial.fiori.2.01/webapp/controller/Master.controller.js
@@ -4,8 +4,9 @@ sap.ui.define([
 	"sap/ui/model/Filter",
 	"sap/ui/model/FilterOperator",
 	'sap/ui/model/Sorter',
-	'sap/m/MessageBox'
-], function (JSONModel, Controller, Filter, FilterOperator, Sorter, MessageBox) {
+	'sap/m/MessageBox',
+	'sap/f/library'
+], function (JSONModel, Controller, Filter, FilterOperator, Sorter, MessageBox,fioriLibrary) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.fiori2.controller.Master", {
@@ -36,6 +37,11 @@ sap.ui.define([
 				oSorter = new Sorter("Name", this._bDescendingSort);
 
 			oBinding.sort(oSorter);
+		},
+		onListItemPress: function () {
+			var oFCL = this.oView.getParent().getParent();
+
+			oFCL.setLayout(fioriLibrary.LayoutType.TwoColumnsMidExpanded);
 		}
 	});
 });
