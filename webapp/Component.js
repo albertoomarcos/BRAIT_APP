058c82853efa347392eabd394049f92511a6985d
Alberto Marcos Blasco <albertoo.marcos@gmail.com>
2019-03-22 12:00:52 +0000

    :warning: No funciona navegación a la otra ruta :warning:

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,9 +1,9 @@
 sap.ui.define([
 "sap/ui/core/UIComponent",
-"sap/ui/model/json/JSONModel"
+"sap/ui/model/json/JSONModel",
+"sap/f/library"
 
-
-], function(UIComponent, JSONModel) {
+], function(UIComponent, JSONModel, fioriLibrary) {
 
 	"use strict";
 
@@ -16,10 +16,13 @@ sap.ui.define([
 		},
 
 		init : function() {
-
+			var oModel,	oRouter;
 			// call the init function of the parent 
 
 			UIComponent.prototype.init.apply(this, arguments);
+			
+			oModel = new JSONModel();
+			this.setModel(oModel);
 
 			// additional initialization can be done here 
 			this.getRouter().initialize();
@@ -29,6 +32,22 @@ sap.ui.define([
 			oProductsModel.loadData("https://openui5.hana.ondemand.com/test-resources/sap/ui/documentation/sdk/products.json");
 			oProductsModel.setSizeLimit(1000);
 			this.setModel(oProductsModel, 'products');
+			
+			oRouter = this.getRouter();
+			oRouter.attachBeforeRouteMatched(this._onBeforeRouteMatched, this);
+			oRouter.initialize();
+		},
+
+		_onBeforeRouteMatched: function(oEvent) {
+			var oModel = this.getModel(),
+				sLayout = oEvent.getParameters().arguments.layout;
+
+			// If there is no layout parameter, set a default layout (normally OneColumn)
+			if (!sLayout) {
+				sLayout = fioriLibrary.LayoutType.OneColumn;
+			}
+
+			oModel.setProperty("/layout", sLayout);
 		}
 	});
 
