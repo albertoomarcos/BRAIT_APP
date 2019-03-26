86d4eb01c46c74f564fa38ebce2622d017c66764
Alberto Marcos Blasco <38374836+albertoomarcos@users.noreply.github.com>
2019-03-26 14:13:58 +0100

    COPIA SEGURIDAD

---
+++ APPMAIN/webapp/index.js
@@ -0,0 +1,21 @@
+sap.ui.define([
+	"sap/m/Button",
+	"sap/m/MessageToast"
+], function (Button, MessageToast) {
+	"use strict";
+
+	new Button({
+		text: "APLICACION HELADOS",
+		press: function () {
+			sap.m.URLHelper.redirect("../../SmartControls/webapp/index.html");
+		}
+	}).placeAt("content");
+	
+	new Button({
+		text: "APLICACION BRAIT",
+		press: function () {
+			sap.m.URLHelper.redirect("../../Brait_app/webapp/index.html");
+		}
+	}).placeAt("content");
+
+});
