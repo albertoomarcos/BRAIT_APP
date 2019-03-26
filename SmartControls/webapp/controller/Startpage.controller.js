68eb64318090fb3aad18dfb10d6880fd62d7a001
Alberto Marcos Blasco <albertoo.marcos@gmail.com>
2019-03-26 09:54:23 +0000

    "Step 2: KPI Tile and Chart Tile on the Start Page" 50%

--- SmartControls/webapp/controller/Startpage.controller.js
+++ SmartControls/webapp/controller/Startpage.controller.js
@@ -4,6 +4,22 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("sap.suite.ui.commons.demokit.tutorial.icecream.01.controller.Startpage", {
-		onInit: function () {}
+		onInit: function () {
+			
+		},
+		getProgress: function (aNodes) {
+			if (!aNodes || aNodes.length === 0) {
+				return 0;
+			}
+			var iSum = 0;
+			for (var i = 0; i < aNodes.length; i++) {
+				iSum += aNodes[i].state === "Positive";
+			}
+			var fPercent = (iSum / aNodes.length) * 100;
+			return fPercent.toFixed(0);
+		},
+		getEntityCount: function (entities) {
+			return entities && entities.length || 0;
+		}
 	});
 });
