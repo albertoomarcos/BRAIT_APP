543d20b7cccebe60b0beaf95aea884ed56034ed0
Alberto Marcos Blasco <albertoo.marcos@gmail.com>
2019-03-26 12:10:21 +0000

    "Step 7": Completado sin errores ✅

---
+++ SmartControls/webapp/controller/ProcessFlow.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/base/strings/formatMessage"
+], function (Controller, formatMessage) {
+	"use strict";
+
+	return Controller.extend("sap.suite.ui.commons.demokit.tutorial.icecream.01.controller.ProcessFlow", {
+		formatMessage: formatMessage,
+
+		onNavButtonPressed: function () {
+			this.getOwnerComponent().getRouter().navTo("home");
+		}
+	});
+});
